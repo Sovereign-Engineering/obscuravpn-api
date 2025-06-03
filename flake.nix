946acd111410c039cd16807233853b0b9979688e
Florian Uekermann <florian@uekermann.me>
2025-06-03 13:05:54 +0200

    update dependencies

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   inputs = {
     crane.url = "github:ipetkov/crane";
     flake-utils.url = "github:numtide/flake-utils";
-    nixpkgs.url = "nixpkgs/nixos-24.11";
+    nixpkgs.url = "nixpkgs/nixos-25.05";
   };
 
   outputs = { self, crane, nixpkgs, flake-utils }:
