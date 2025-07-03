7f1558a8be2b4c24ddfc586517d0895ab6465445
Francesca Lovebloom <francesca@sovereign.engineering>
2025-07-03 14:26:43 -0700

    Fix test

--- src/cmd/account/info.rs
+++ src/cmd/account/info.rs
@@ -17,8 +17,10 @@ fn test_account_info_json() {
         {
           "id": "0000000000000000000",
           "active": true,
+          "active_reason": "TopUp",
           "top_up": { "credit_expires_at": 1000 },
-          "subscription": null
+          "subscription": null,
+          "app_store_subscription": null
         }
     "#;
     crate::cmd::check_cmd_json::<GetAccountInfo>(None, Some(output_json));
