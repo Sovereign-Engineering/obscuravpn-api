b0d8575e09aa4ce2501b1b45481b09041a1ec4bc
Francesca Lovebloom <francesca@sovereign.engineering>
2025-10-20 17:23:06 -0700

    Account deletion
    Contributes to https://linear.app/soveng/issue/OBS-2476/ios-account-deletion

---
+++ src/cmd/account/delete.rs
@@ -0,0 +1,15 @@
+use crate::cmd::Cmd;
+use serde::{Deserialize, Serialize};
+
+#[derive(Clone, Debug, Deserialize, Serialize)]
+pub struct DeleteAccountOutput {}
+
+#[derive(Clone, Debug, Deserialize, Serialize)]
+pub struct DeleteAccount {}
+
+impl Cmd for DeleteAccount {
+    type Output = DeleteAccountOutput;
+
+    const METHOD: http::Method = http::Method::DELETE;
+    const PATH: &'static str = super::PATH;
+}
